bda5b2023ca4b2901a5175ea65458ee033b747ae
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-07-03 00:19:14 +0800

    Add theme_ilo

---
+++ R/theme_ilo.R
@@ -0,0 +1,17 @@
+theme_ilo <- function(){
+    theme_minimal() +
+        theme(
+            # text = element_text(family = "Bookman", color = "gray25"),
+            plot.subtitle = element_text(size = 9),
+            plot.caption = element_text(color = "gray30"),
+            # plot.background = element_rect(fill = "gray95"),
+            plot.margin = unit(c(5, 10, 5, 10), units = "mm"),
+            axis.title.x = element_text(size=12,face = "bold"),
+            axis.title.y = element_text(size=12,face = "bold"),
+            # x和y的命名，要加粗，ppt才好看
+            axis.text.x = element_text(size=7, angle = 70, hjust = 1),
+            # 控制axis字体大小，7号大小最好
+            axis.text.y = element_text(size=7),
+            legend.title=element_blank()
+        )
+}
